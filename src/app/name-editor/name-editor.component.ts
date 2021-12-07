075ae972a29b4bd5fb1beda1f2ab4537a0cf733c
flobev <florian.bevierre@gmail.com>
2021-12-07 21:34:30 +0100

    Ajout du module ReactiveFormsModule

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-name-editor',
   templateUrl: './name-editor.component.html',
   styleUrls: ['./name-editor.component.scss']
 })
-export class NameEditorComponent implements OnInit {
+export class NameEditorComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  name = new FormControl('');
 }
